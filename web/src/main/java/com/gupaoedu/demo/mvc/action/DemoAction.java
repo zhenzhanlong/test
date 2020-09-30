8de6ff569686e19aa2571bb75eba7f826d7fea5d
complex <zhanlong1988@126.com>
2020-09-30 14:44:53 +0800

    提交测试

--- web/src/main/java/com/gupaoedu/demo/mvc/action/DemoAction.java
+++ web/src/main/java/com/gupaoedu/demo/mvc/action/DemoAction.java
@@ -1,18 +1,19 @@
 package com.gupaoedu.demo.mvc.action;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.gupaoedu.demo.service.IDemoService;
 import com.gupaoedu.mvcframework.annotation.GPAutowired;
 import com.gupaoedu.mvcframework.annotation.GPController;
 import com.gupaoedu.mvcframework.annotation.GPRequestMapping;
 import com.gupaoedu.mvcframework.annotation.GPRequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 //虽然，用法一样，但是没有功能,在线编辑测试文件冲突
+
+//虽然，用法一样，但是没有功能提交测试
 @GPController
 @GPRequestMapping("/demo")
 public class DemoAction {
