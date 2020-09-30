b0c60759cf53fe3e3c365e85f9439af0a3c07648
complex <zhanlong1988@126.com>
2020-09-30 14:55:18 +0800

    提交测试

--- web/src/main/java/com/gupaoedu/mvcframework/annotation/GPService.java
+++ web/src/main/java/com/gupaoedu/mvcframework/annotation/GPService.java
@@ -1,7 +1,7 @@
 package com.gupaoedu.mvcframework.annotation;
 
 import java.lang.annotation.*;
-//哈桑·怀特塞德
+//哈桑·怀特塞德，文件再次更新
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
